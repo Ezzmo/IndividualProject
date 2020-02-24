d30130c9302f5402f6882f48c49098a750979984
Ubuntu <Ezzmo@FlaskWorkstation.revbhzbnmzoepha0wgc0v1sekc.cwx.internal.cloudapp.net>
2020-02-24 12:02:04 +0000

    templates added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Welcome to Footrumps"
+    user = {'username': 'Mo'}
+    posts =[
+            {'author' : {'username' : 'Chief'},
+             'body' : 'Anyone wanna play me?'
+            },
+            {'author' : {'username' : 'Merkleman'},
+             'body' : 'Im definitely the best'
+            }     
+        ]
+    return render_template('home.html', title='Home', user=user, posts=posts)
+
