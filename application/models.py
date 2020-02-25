4dda590ff7d5c61e2a69b1cb32408ec414359ff4
Ubuntu <Ezzmo@Flaskworkstation.revbhzbnmzoepha0wgc0v1sekc.cwx.internal.cloudapp.net>
2020-02-25 15:59:00 +0000

    Got forms and posts from database working

--- application/models.py
+++ application/models.py
@@ -4,14 +4,18 @@ class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
-    title = db.Column(db.String(100), nullable = False)
-    content = db.Column(db.String(1000), nullable = False)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    content = db.Column(db.String(500), nullable=False, unique=True)
 
     def __repr__(self):
-        return ''.join(['User: ', self.first_name, ' ', self.last_name, '\r\n',
-                        'Title: ', self.title, '\r\n', self.content
-                ])
+        return ''.join([
+            'User: ', self.first_name, ' ', self.last_name, '\r\n',
+            'Title: ', self.title, '\r\n', self.content
+            ])
 
+class Users(db.Model):
+    Username = db.Column(db.String(15), primary_key=True)
+    Password = db.Column(db.String(30), nullable = False)
 class Players(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(50), nullable=False, unique=True)
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from application import app
-
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.models import Posts
+from application.forms import PostForm
 @app.route('/')
 @app.route('/home')
 def home():
@@ -14,4 +15,25 @@ def home():
             }     
         ]
     return render_template('home.html', title='Home', user=user, posts=posts)
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+                first_name = form.first_name.data,
+                last_name = form.last_name.data,
+                title = form.title.data,
+                content = form.content.data
+            )
+        db.session.add(postData)
+        db.session.commit()
 
+        return redirect(url_for('home'))
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
+@app.route('/board')
+def board():
+    posts = Posts.query.all()[::-1]
+    return render_template('postboard.html', posts=posts)
